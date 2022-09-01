745d75831c009e366e3440235d1127f55f7a54a2
jiyoung <remi1217@naver.com>
2022-09-01 21:24:54 +0900

    1205_등수구하기

---
+++ 2. silver/1205_등수구하기.py
@@ -0,0 +1,17 @@
+# 입력
+N, score, P = map(int, input().split())
+if N:
+    lst = list(map(int, input().split()))
+
+    # 풀이
+    if N == P and lst[-1] >= score:
+        print(-1)
+    else:
+        ans = N + 1
+        for i in range(N):
+            if lst[i] <= score:
+                ans = i + 1
+                break
+        print(ans)
+else:
+    print(1)
